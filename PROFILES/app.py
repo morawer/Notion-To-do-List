5b99e65ef5d8b1d07d5d36bc86d49cf662f49f7e
morawer <morala84@gmail.com>
2022-12-04 12:00:54 +0100

    List of profiles created

--- PROFILES/app.py
+++ PROFILES/app.py
@@ -14,7 +14,8 @@ def fileSelection():
 
 try:
     excel = fileSelection()
-    wb = load_workbook(fileSelection)
+    print(excel)
+    wb = load_workbook(excel)
 except:
     print("ERROR - PROCESS CANCELED")
     quit()
@@ -35,6 +36,7 @@ for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
     item_value = str(row[2].value)
     name_value = row[3].value
     qty_value = row[4].value
+    distance_value = row[9].value
 
     co_value = row[0].value
     line_value = row[1].value
@@ -47,16 +49,17 @@ for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
         line = row[1].value
         qty = qty + 1
 
-        wb_panels = load_workbook("Plantilla Paneles.xlsx")
+        wb_profiles = load_workbook(
+            "/home/dani/Projects/To_do_List_Excel/PROFILES/Plantilla Perfiles.xlsx")
 
-        ws_panels = wb_panels.active
-        ws_panels["B2"].value = f"{co}-{line}"
+        ws_profiles = wb_profiles.active
+        ws_profiles["B2"].value = f"{co}-{line}"
         row_counter = 0
         
-    ws_panels.merge_cells(start_row=11 + row_counter,
+    ws_profiles.merge_cells(start_row=11 + row_counter,
                           start_column=2, end_row=11 + row_counter, end_column=3)
     
-    ws_panels.merge_cells(start_row=11 + row_counter,
+    ws_profiles.merge_cells(start_row=11 + row_counter,
                           start_column=4, end_row=11 + row_counter, end_column=9)
 
     thin_border = Border(left=Side(style='thin'),
@@ -70,57 +73,57 @@ for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
     
     
 
-    ws_panels.cell(row=11 + row_counter, column=2, 
+    ws_profiles.cell(row=11 + row_counter, column=2, 
                    value=item_value).alignment=cell_alignment
-    ws_panels.cell(row=11 + row_counter, column=4,
+    ws_profiles.cell(row=11 + row_counter, column=4,
                    value=name_value).alignment = cell_alignment
-    ws_panels.cell(row=11 + row_counter, column=10,
+    ws_profiles.cell(row=11 + row_counter, column=10,
                    value=qty_value).alignment = cell_alignment
+    ws_profiles.cell(row=11 + row_counter, column=11,
+                   value=distance_value[0:4]).alignment = cell_alignment
 
-    ws_panels.cell(row=11 + row_counter, column=2).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=3).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=4).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=5).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=6).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=7).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=8).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=9).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=10).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=11).border = thin_border
-    ws_panels.cell(row=11 + row_counter, column=12).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=2).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=3).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=4).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=5).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=6).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=7).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=8).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=9).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=10).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=11).border = thin_border
+    ws_profiles.cell(row=11 + row_counter, column=12).border = thin_border
 
-    if item_value.__contains__("S"):
+            
+    if item_value[0:3] == "490":
         id_line = 1
-        
-    elif item_value[0:3] == "503":
-        id_line = 2
        
-    elif item_value[0:3] == "506":
+    elif item_value[0:3] == "491":
+        id_line = 2
+
+    elif item_value[0:3] == "492":
         id_line = 3
 
-    elif item_value[0:2] != "50":
-        id_line = 5
+    elif item_value[0:3] == "493":
+        id_line = 4
 
-    elif item_value[0:3] == "500" and not item_value.__contains__("S"):
-        id_line = 6
-
-    if (id_line%2 != 0):
-        ws_panels.cell(row=11 + row_counter, column=2).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=3).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=4).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=5).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=6).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=7).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=8).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=9).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=10).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=11).fill= my_fill
-        ws_panels.cell(row=11 + row_counter, column=12).fill= my_fill
+    if (id_line%2 == 0):
+        ws_profiles.cell(row=11 + row_counter, column=2).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=3).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=4).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=5).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=6).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=7).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=8).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=9).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=10).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=11).fill= my_fill
+        ws_profiles.cell(row=11 + row_counter, column=12).fill= my_fill
         
     row_counter = row_counter + 1
 
     try:
-        wb_panels.save(f"{co_value}-{line_value}-PANELES.xlsx")
+        wb_profiles.save(f"{co_value}-{line_value}-PERFILES.xlsx")
 
     except:
         print(f"{co_value}-{line_value} --- ERROR")
